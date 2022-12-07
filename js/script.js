06032bb9b4c53999a06c13762bdc3db8eecec639
Manuel Garcia-Nieto <manuel@WebDev.local>
2022-12-07 21:50:34 +0100

    Add payment info section

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@
 /* DOM References
 --------------------------------------------------------- */
 const nameInput = document.getElementById('name')
+const emailInput = document.getElementById('email')
 const otherJobRoleInput = document.getElementById('other-job-role')
 const jobRoleSelect = document.getElementById('title')
 const designSelect = document.getElementById('design')
@@ -13,6 +14,14 @@ const colorSelectOptions = document.querySelectorAll('#color option')
 const activitiesFieldset = document.getElementById('activities')
 const activitiesTotalCost = document.getElementById('activities-cost')
 const checkBoxes = document.querySelectorAll('input[type=checkbox]')
+const paymentMethod = document.getElementById('payment')
+const creditCardMethod = document.getElementById('credit-card')
+const paypalMethod = document.getElementById('paypal')
+const bitcoinMethod = document.getElementById('bitcoin')
+const cardNumber = document.getElementById('cc-num')
+const zipCode = document.getElementById('zip')
+const cvv = document.getElementById('cvv')
+const form = document.querySelector('form')
 
 /* Variables
 ---------------------------------------------------------- */
@@ -64,7 +73,7 @@ designSelect.addEventListener('change', (e) => {
 })
 
 /**
- *
+ * Adds up the total cost of the activities and prevents the user from selecting incompatible activities.
  * @param  {event type} 'change'
  * @param  {listener} (e)
  */
@@ -89,7 +98,7 @@ activitiesFieldset.addEventListener('change', (e) => {
 })
 
 /**
- * Sets the focus on the selected checkbox or removes it.
+ * Sets the focus state on the selected checkbox or removes it.
  * @param  {iterator} checkBox
  */
 for (const checkBox of checkBoxes) {
@@ -100,3 +109,34 @@ for (const checkBox of checkBoxes) {
     e.target.parentElement.classList.remove('focus')
   })
 }
+
+// Hides paypal and bitcoin method and sets credit card method default.
+paypalMethod.setAttribute('hidden', true)
+bitcoinMethod.setAttribute('hidden', true)
+paymentMethod.selectedIndex = 1
+
+/**
+ * Displays info according to selected payment method.
+ * @param  {event type} 'change'
+ * @param  {listener} e
+ */
+paymentMethod.addEventListener('change', (e) => {
+  if (e.target.value === creditCardMethod.id) {
+    creditCardMethod.removeAttribute('hidden')
+    paypalMethod.setAttribute('hidden', true)
+    bitcoinMethod.setAttribute('hidden', true)
+  } else if(e.target.value === paypalMethod.id){
+    paypalMethod.removeAttribute('hidden')
+    creditCardMethod.setAttribute('hidden', true)
+    bitcoinMethod.setAttribute('hidden', true)
+  }else{
+    bitcoinMethod.removeAttribute('hidden')
+    creditCardMethod.setAttribute('hidden', true)
+    paypalMethod.setAttribute('hidden', true)
+  }
+})
+
+
+form.addEventListener('submit', (e) => {
+   e.preventDefault()
+})
